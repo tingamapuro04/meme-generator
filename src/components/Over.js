d3ed84921de4d21060dfeb0ffc2ca4bdb6b05a51
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-21 16:31:25 +0300

    Complete form

---
+++ src/components/Over.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Over() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Over
--- src/components/Tems.js
+++ src/components/Tems.js
@@ -2,21 +2,46 @@ import React, { useState } from 'react'
 
 function Tems() {
   const [formdata, setFormData] = useState(
-    {first: "", second: "", email: ""}
+    {first: "", second: "", email: "", area: "", check: true}
   );
   console.log(formdata)
   const handleChange = (e) => {
+    const {name, value, type, checked} = e.target
     e.preventDefault()
     setFormData(prev => {
       return {
         ...prev,
-        [e.target.name]: e.target.value
+        [name]: type === "checkbox" ? checked : value
       }
     })
   }
+
+  
+
+
+
   return (
     <>
       <form>
+
+        <input
+        type="radio"
+        id="Unemployed"
+        name="radioBtn"
+         />
+
+         <label htmlFor="Unemployed">Bila kazi</label>
+
+
+
+        <input
+        type="checkbox"
+        id= "isFriendly"
+        checked={formdata.check}
+        onChange={handleChange}
+        name="isFriendly"
+        />
+        <label htmlFor="isFriendly" >are you friendly</label>
         <input
          type="text"
          placeholder="people"
@@ -38,6 +63,7 @@ function Tems() {
          name="email"
          value={formdata.email}
          />
+         <textarea name="area" value={formdata.area} onChange={handleChange} />
       </form>
     </>
   )
