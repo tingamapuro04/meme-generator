d7456d89a1fa9ef9271a5941323919b3b30562bd
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-21 20:43:51 +0300

    first cleanup

--- src/App.js
+++ src/App.js
@@ -2,33 +2,16 @@ import React, {useState} from 'react';
 import './App.css';
 import Form from './components/Form';
 import Header from './components/Header';
-import Nonsense from './components/Nonsense';
-import troon from './components/Box'
-import On from './components/On';
-import Over from './components/Over';
-import Api from './components/Api';
 
 function App() {
 
 
-  
 
 
-  const [first, setFirst] = useState(troon);
-
-
-
-  const elements = first.map(item => (
-    <On on={item.on} key={item.id} ff={item.id} />
-    ))
   return (
     <div className="App">
       <Header />
       <Form />
-      {elements}
-      <Nonsense setup={'mobutu'} punchline={'diego'} />
-      <Over />
-      <Api />
     </div>
   );
 }
